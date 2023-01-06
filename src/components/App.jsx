359f7335dff55eb5d3dc76d5a61b2336855bdfc1
Volin13 <110535249+Volin13@users.noreply.github.com>
2023-01-06 19:51:31 +0200

    commit message

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
+// import { useEffect, useState } from 'react';
+import {
+  Section,
+  Filter,
+  Contacts,
+  AddContactForm,
+  Container,
+} from '../components/index';
+// import { nanoid } from 'nanoid';
+
+const App = () => {
+  // const [contacts, setContacts] = useState(() => {
+  //   const savedData = localStorage.getItem('contacts');
+  //   const parsedData = JSON.parse(savedData);
+  //   return parsedData ?? [];
+  // });
+  // const [filter, setFilter] = useState('');
+
+  // useEffect(() => {
+  //   const seveData = JSON.stringify(contacts);
+  //   return localStorage.setItem('contacts', seveData);
+  // }, [contacts]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container title="Phone book">
+      <Section title="Add new contact" variant="h3">
+        <AddContactForm />
+      </Section>
+      <Section title="Your contacts" variant="h3">
+        <Contacts>
+          <Filter />
+        </Contacts>
+      </Section>
+    </Container>
   );
 };
+export default App;
